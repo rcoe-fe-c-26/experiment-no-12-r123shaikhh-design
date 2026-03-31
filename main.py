297733964a7ec5c3e08d19f352cb56eb2b29b6d6
Rehan Shaikh <r.123shaikhh@gmail.com>
2026-03-31 15:55:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 # AIM: Develop a Python program that reads a text file and 
 # prints words of specified lengths (e.g., three, four, 
 # five, etc.) found within the file.
-# Coder:
-# Date:
-
+# Coder:REHAN AKHTAR ALI SHAIKH
+# Date:30/03/26
 print("--- Extracting Words from Text File ---\n")
-
+num = int(input("enter word length: "))
+words =[]
+with open("story.txt", "r") as file:
+    content = file.read().split()
+    for i in content:
+        if len(i) == num:
+            words.append(i)
+words=set(words)
+words=list(words)
+words.sort()
+print(words)
